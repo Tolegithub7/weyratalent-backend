92928051ea7853c2f422c582ef52bf240124a484
poseidon2022 <kidusm82@gmail.com>
2025-02-13 00:51:50 +0300

    feat: setup service response and swagger

---
+++ src/api-docs/openAPIDocumentGenerator.ts
@@ -0,0 +1,25 @@
+import { OpenAPIRegistry, OpenApiGeneratorV3 } from "@asteasolutions/zod-to-openapi";
+
+export function generateOpenAPIDocument() {
+  const registry = new OpenAPIRegistry([]);
+  registry.registerComponent("securitySchemes", "bearerAuth", {
+    type: "http",
+    scheme: "bearer",
+  });
+  const generator = new OpenApiGeneratorV3(registry.definitions);
+
+  return generator.generateDocument({
+    openapi: "3.0.0",
+    info: {
+      version: "1.0.0",
+      title: "Weyra-Talent Backend API",
+      description: "API Documentation for Weyra-Talent Backend API",
+    },
+    // add bearerAuth to security for every path except /health-check
+    security: [{ bearerAuth: [] }],
+    externalDocs: {
+      description: "View the raw OpenAPI Specification in JSON format",
+      url: "/swagger.json",
+    },
+  });
+}
--- src/server.ts
+++ src/server.ts
@@ -1,10 +1,12 @@
 import cors from "cors";
 import express, { type Express } from "express";
 import { pino } from "pino";
+import { openAPIRouter } from "./api-docs/openAPIRouter";
 
 const app: Express = express();
 const logger = pino({ name: "server start" });
 app.use(cors());
 app.use(express.json());
+app.use(openAPIRouter);
 
 export { app, logger };
